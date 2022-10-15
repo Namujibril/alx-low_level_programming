27df405eefa84564d3f693eaf71291ddb4345f24
Namujibril <Namujibril@gmail.com>
2022-10-15 10:05:29 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
-
 /**
- * main - program that prints all possible combinations of
- * two two-digit
- * The numbers should range from 0 to 99
- * Numbers must be separated by ,followed by a space
- * All numbers should be printed with two digits. 1 should be printed as 01
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-{
-	int n1 = 48;
-	int a = 0;
-	int b;
-	int com = 44;
 
-	while (a <= 99)
-	{
-		b = a + 1;
-		while (b <= 99)
-		{
-			putchar((a / 10) + n1);
-			putchar((a % 10) + n1);
-			putchar(32);
-			putchar((b / 10) + n1);
-			putchar((b % 10) + n1);
-			if (a != 98 || b != 99)
-			{
-				putchar(com);
-				putchar(32);
-			}
-			b += 1;
-		}
-		a += 1;
-	}
-	putchar('\n');
-	return (0);
+{
+int first_nums; /*Decarling statement*/
+int second_nums;
+
+/*for 0-9 with ASCII*/
+for (first_nums = 0; first_nums < 100 ; first_nums++)
+{
+for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
+{
+
+putchar (first_nums / 10 + '0'); /*print  first nums, first digit*/
+putchar (first_nums % 10 + '0'); /*print  first nums, second digit*/
+
+putchar (' '); /*prirint a space*/
+
+putchar (second_nums / 10 + '0'); /*print  sencond nums, first digit*/
+putchar (second_nums % 10 + '0'); /*print  second nums, second digit*/
+
+/*break program if the condition is true (if is 98 99)*/
+if (first_nums == 98 && second_nums == 99)
+{
+break;
 }
+
+putchar(','); /*print (,)*/
+putchar (' '); /*prirint a space*/
+
+} /*end for #2*/
+
+} /*end for #1*/
+
+putchar('\n'); /*new line*/
+
+return (0);

afa7d3804deba68a74c5072106ea45ba8cb9c17a
Namujibril <Namujibril@gmail.com>
2022-10-19 20:51:13 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,5 +1,11 @@
-#include <unisdt.h>
-
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes character c to stdout
+ *
+ * Return: on success 1
+ *
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
